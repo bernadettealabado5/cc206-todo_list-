a9c55e86d7a66badffb8b3a1297811e1824f8c25
bernadettealabado5 <bernadette.alabado@wvsu.edu.ph>
2023-10-03 13:57:48 +0800

    Added a feature folder and widgets

---
+++ lib/features/sign_up_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class signup extends StatelessWidget {
+  const signup({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: [
+       TextFormField()
+      ],)
+
+    );
+  }
+}
+
