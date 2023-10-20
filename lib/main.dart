cffc8d032759cde1d9cbc89a765799d069cdca55
bernadettealabado5 <bernadette.alabado@wvsu.edu.ph>
2023-10-20 13:47:03 +0800

    Added widgets for login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:cc206_todo_list_/features/sign_up_page.dart';
 import 'package:flutter/material.dart';
+import 'package:cc206_todo_list_/features/sign_up_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const signup(),
+      home: LoginPage(),
     );
   }
 }
